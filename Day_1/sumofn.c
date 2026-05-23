20dcb5491ebc4c8eb0b9013dcc3c6c7812e9336a
Aakash yadav <aakashyadav3400@gmail.com>
2026-05-23 14:00:50 +0530

    chane

--- Day_1/sumofn.c
+++ Day_1/sumofn.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main() {
-   int n, sum = 0;
-   printf("Enter a positive integer: ");
+ int n , s=0;
+   printf("ENTER A NUMBER n");
    scanf("%d", &n);
-   for (int i = 1; i <= n; i++) {
-       sum += i; 
+   for(int i =1;i<=n;i++){
+      s=s+i;
    }
-   printf("Sum of first %d natural numbers is: %d\n", n, sum);
+   printf("sum = %d",s);
    return 0;
 }
