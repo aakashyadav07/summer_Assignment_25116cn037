79edabe88bc2c6856f646e7537f904c738d40152
Aakash yadav <aakashyadav3400@gmail.com>
2026-06-14 17:10:44 +0530

    .

---
+++ Day_12/pallindrome.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int palindrome(int n)
+{ int rev = 0, temp = n, rem;
+ while (n > 0)
+    {
+        rem = n % 10;
+        rev = rev * 10 + rem;
+        n = n / 10;  } 
+          if (temp == rev)
+        return 1;
+    else
+        return 0;}
+int main()
+{   int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+   if (palindrome(num))
+        printf("Palindrome");
+    else
+        printf("Not Palindrome");
+  return 0;
+}
